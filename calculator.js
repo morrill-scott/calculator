fc372cb8523c03631fa31c8506ffa81726995e03
english-31 <185120814+english-31@users.noreply.github.com>
2024-12-31 12:52:54 -0600

    Add Event Listeners to Buttons
    Added events listeners to buttons to enact different fucntions and returns.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
 const add = function ( a , b) {
-    return a + b
+    return Number(a) + Number(b);
 }
 
 const subtract  = function ( a , b ) {
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 const multiply = function ( a , b ) {
@@ -24,3 +24,31 @@ const operate = function( num1, operator, num2) {
     if(operator === '*') return multiply( num1, num2 );
     if(operator === '/') return divide( num1, num2 );
 }
+
+const numBtns = document.querySelectorAll(".number");
+const clearBtn = document.querySelector("#clear");
+const opBtns = document.querySelectorAll(".operator")
+const eqBtn = document.querySelector("#equals");
+const displayContent = document.querySelector("#screen");
+
+numBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        let numStore = button.id;
+        displayContent.textContent = displayContent.textContent + numStore;
+    });
+});
+
+opBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        let opStore = (button.id).toString();
+        displayContent.textContent = displayContent.textContent + opStore;
+    })
+})
+
+clearBtn.addEventListener("click",() => displayContent.textContent = "");
+
+eqBtn.addEventListener("click",() => {
+    expression = (displayContent.textContent).split('');
+    alert(expression);
+    displayContent.textContent = operate(expression[0],expression[1],expression[2]);
+});
