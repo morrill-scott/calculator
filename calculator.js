2a223ca4a1558d964148870e27a0c9fdc2c22397
english-31 <185120814+english-31@users.noreply.github.com>
2024-12-31 10:15:46 -0600

    Added operate functon
    Takes 2 number arguments and an operator.
    Calls upon different fucntions for different math operations depending
    on the value of the operator
    
    One thing that may need to be fixed in the future, is that the operator
    when typed into the console doesn't log as a string and the function needs
    the operator to be a string in order to function.

--- calculator.js
+++ calculator.js
@@ -13,3 +13,14 @@ const multiply = function ( a , b ) {
 const divide = function ( a , b ) {
     return a / b;
 }
+
+let num1;
+let operator;
+let num2;
+
+const operate = function( num1, operator, num2) {
+    if(operator === '+') return add( num1, num2 );
+    if(operator === '-') return subtract( num1, num2 );
+    if(operator === '*') return multiply( num1, num2 );
+    if(operator === '/') return divide( num1, num2 );
+}
